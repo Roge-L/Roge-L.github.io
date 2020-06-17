86d81099fd9de1edc306bdae0c274bec33984e4a
Roger Lam <roger.l.jiahao2@gmail.com>
2020-06-17 12:53:49 -0400

    Finish raw version of the website
    The side navigation and cross page traversal allows for
    advancement into detailing stage.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
-function myFunction() {
-    console.log('responsive')
+const navToggle = document.getElementById('dropbtn')
+let sidenavOpen = false
 
-}
+navToggle.addEventListener('click', () => {
+    if (sidenavOpen == false) {
+        document.getElementById('sidenav-links').style.transform = 'translateX(0)';
+        sidenavOpen = true
+    } else {
+        document.getElementById('sidenav-links').style.transform = 'translateX(-100%)';
+        sidenavOpen = false
+    }
+})
