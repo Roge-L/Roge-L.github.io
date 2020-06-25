1951281fee214524c14daa5d18158337e644846a
Roger Lam <roger.l.jiahao2@gmail.com>
2020-06-25 16:20:35 -0400

    Move Barba Init Back into Index

--- js/index.js
+++ js/index.js
@@ -1,17 +1,4 @@
-barba.init({
-    transitions: [{
-        name: 'opacity-transition',
-        leave(data) {
-          console.log('working');
-        },
-        enter(data) {
-          console.log('working')
-        }
-    }]
-  })
-
-  var tl = gsap.timeline();
-
-  tl.from('#home-page-title', {duration: 1, y: -30, opacity: 0});
-  tl.from('#home-page-subtitle-1', {duration: 0.5, y: 10, opacity: 0});
-  tl.from('#home-page-subtitle-2', {duration: 0.5, y: 10, opacity: 0})
+var tl = gsap.timeline();
+tl.from('#home-page-title', {duration: 1, y: -30, opacity: 0});
+tl.from('#home-page-subtitle-1', {duration: 0.5, y: 10, opacity: 0});
+tl.from('#home-page-subtitle-2', {duration: 0.5, y: 10, opacity: 0})
