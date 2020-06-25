af9348b74a5ccb15b54c738c1e486f5706fa07de
Roger Lam <roger.l.jiahao2@gmail.com>
2020-06-25 14:09:00 -0400

    Change back to CDN for BarbaJS

--- js/index.js
+++ js/index.js
@@ -1,12 +1,17 @@
-const navToggle = document.getElementById('dropbtn')
-let sidenavOpen = false
+barba.init({
+    transitions: [{
+        name: 'opacity-transition',
+        leave(data) {
+          console.log('working');
+        },
+        enter(data) {
+          console.log('working')
+        }
+    }]
+  })
 
-navToggle.addEventListener('click', () => {
-    if (sidenavOpen == false) {
-        document.getElementById('sidenav-links').style.transform = 'translateX(0)';
-        sidenavOpen = true
-    } else {
-        document.getElementById('sidenav-links').style.transform = 'translateX(-100%)';
-        sidenavOpen = false
-    }
-})
+  var tl = gsap.timeline();
+
+  tl.from('#home-page-title', {duration: 1, y: -30, opacity: 0});
+  tl.from('#home-page-subtitle-1', {duration: 0.5, y: 10, opacity: 0});
+  tl.from('#home-page-subtitle-2', {duration: 0.5, y: 10, opacity: 0})
---
+++ js/main.js
@@ -0,0 +1,12 @@
+const navToggle = document.getElementById('dropbtn')
+let sidenavOpen = false
+
+navToggle.addEventListener('click', () => {
+    if (sidenavOpen == false) {
+        document.getElementById('sidenav-links').style.transform = 'translateX(0)';
+        sidenavOpen = true
+    } else {
+        document.getElementById('sidenav-links').style.transform = 'translateX(-100%)';
+        sidenavOpen = false
+    }
+})
