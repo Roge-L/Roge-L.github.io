1611a6b2cb1209d2eef3d8379e934d01663abbcc
Roger Lam <roger.l.jiahao2@gmail.com>
2020-06-25 18:34:28 -0400

    Remove BarbaJS
    Couldn't solve installation issue so will put page transitions to
    the side in the meantime.  Enables me to work on content and other
    design aspects of the website.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-var tl = gsap.timeline();
-tl.from('#home-page-title', {duration: 1, y: -30, opacity: 0});
-tl.from('#home-page-subtitle-1', {duration: 0.5, y: 10, opacity: 0});
-tl.from('#home-page-subtitle-2', {duration: 0.5, y: 10, opacity: 0})
+document.addEventListener('click', () => {
+    window.location.href = 'profile.html'
+})
