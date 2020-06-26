fc85a4afd846c69066e0f70f7463eb54319d4403
Roger Lam <roger.l.jiahao2@gmail.com>
2020-06-26 18:22:30 -0400

    Add Rough Profile Content and Design

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
+gsap.from('.home-page-text', {duration: 1.5, opacity: 0})
+gsap.from('#home-page-subtitle-1, #home-page-subtitle-2', {y: 20})
+gsap.from('#home-page-title', {x: 20})
+
 document.addEventListener('click', () => {
     window.location.href = 'profile.html'
 })
