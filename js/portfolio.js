c36531fa39685a82145e7321a243a687adc62790
Roger Lam <roger.l.jiahao2@gmail.com>
2020-07-16 15:17:13 -0400

    Implement Foundation CSS and Develop Base Design
    With a CSS framework, the website becomes responsive and easier to
    develop.  This pushed the learning of frameworks and web dev
    entirely.

---
+++ js/portfolio.js
@@ -0,0 +1,15 @@
+document.getElementById("proj1").addEventListener("click", () => {
+    window.location.href = '#proj1';
+})
+
+document.getElementById("proj2").addEventListener("click", () => {
+    window.location.href = '#proj2';
+})
+
+document.getElementById("proj3").addEventListener("click", () => {
+    window.location.href = '#proj3';
+})
+
+document.getElementById("proj4").addEventListener("click", () => {
+    window.location.href = '#proj4';
+})
